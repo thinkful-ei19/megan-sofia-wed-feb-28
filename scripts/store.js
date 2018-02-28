e10efcfa99a4ad74002339bc31f746e12b38fb85
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-02-28 14:19:22 -0500

    Working on instruction 7, passing over to Megan

--- scripts/store.js
+++ scripts/store.js
@@ -16,24 +16,24 @@ const store = (function(){
     return this.items.find(item => item.id === id);
   };
 
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
-  };
+  //const findAndToggleChecked = function(id) {
+  //  const item = this.findById(id);
+  //  item.checked = !item.checked;
+  //};
 
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
   };
 
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
-  };
+  //const findAndUpdateName = function(id, name) {
+  //  try {
+  //    Item.validateName(name);
+  //    const item = this.findById(id);
+  //    item.name = name;
+  //  } catch(e) {
+  //    console.log('Cannot update name: ' + e.message);
+  //  }
+  //};
 
   const toggleCheckedFilter = function() {
     this.hideCheckedItems = !this.hideCheckedItems;
@@ -50,9 +50,9 @@ const store = (function(){
 
     addItem,
     findById,
-    findAndToggleChecked,
+    //findAndUpdateName
     findAndDelete,
-    findAndUpdateName,
+    //findAndToggleChecked,
     toggleCheckedFilter,
     setSearchTerm,
   };
